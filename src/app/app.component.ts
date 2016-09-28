9b42b6c1255818ef9cab03aaa5768e648c0f40c3
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-09-28 21:23:33 +0200

    just a tiny little bit css! ;-)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Release } from './shared/release';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
 
