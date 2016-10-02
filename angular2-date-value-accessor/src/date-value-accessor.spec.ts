4bb206313e7890658169055db242c4e300f3c80a
JohannesHoppe <johannes.hoppe@haushoppe-its.de>
2016-10-02 20:59:41 +0200

    wip

--- angular2-date-value-accessor/src/date-value-accessor.spec.ts
+++ angular2-date-value-accessor/src/date-value-accessor.spec.ts
@@ -1,10 +1,48 @@
-import 'es6-shim';
-import 'reflect-metadata';
+import { async, TestBed, ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { Component } from '@angular/core';
 
-describe('test', () => {
+import { DateValueAccessor } from './date-value-accessor';
 
-  it('test', () => {
-    expect(true).toBe(true);
+
+@Component({
+  template: `
+  <form>
+    <input type="date" name="test1" [(ngModel)]="testDate1">
+    <input type="date" name="test2" [(ngModel)]="testDate2" useValueAsDate>
+  </form>`
+})
+export class TestFormComponent {
+  testDate1: Date;
+  testDate2: Date;
+  constructor() {
+    this.testDate1 = new Date('2016-07-22');
+    this.testDate2 = new Date('2016-09-15');
+  }
+}
+
+describe('DateValueAccessor', () => {
+  let fixture: ComponentFixture<TestFormComponent>;
+  let formComponent: TestFormComponent;
+
+  beforeEach(async(() => {
+
+    TestBed.configureTestingModule({
+      declarations: [TestFormComponent, DateValueAccessor],
+      imports: [FormsModule]
+    });
+
+    TestBed.compileComponents();
+
+    fixture = TestBed.createComponent(TestFormComponent);
+    formComponent = fixture.componentInstance;
+  }));
+
+  it('should fix to date input controls', () => {
+
+    //fixture.debugElement.query(By.directive(DateValueAccessor));
+
   });
 
 });
