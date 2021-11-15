8eb40dd41f140de80d0bd6d26d93810155da753b
Johannes Hoppe <johannes.hoppe@haushoppe-its.de>
2021-11-15 11:01:03 +0100

    typo

--- workspace/projects/date-value-accessor/src/lib/local-date-value-accessor.spec.ts
+++ workspace/projects/date-value-accessor/src/lib/local-date-value-accessor.spec.ts
@@ -20,11 +20,11 @@ describe('LocalDateValueAccessor (template-driven forms)', () => {
   setupTemplateDrivenForms(c, TestFormComponent, LocalDateValueAccessor);
 
   it('should fix date input controls to bind on dates', waitForAsync(() => {
-    expect(c.inputElement.nativeElement.value).toBe('2020-12-08');
+    expect(c.inputElement.value).toBe('2020-12-08');
   }));
 
   it('should populate LOCAL dates (instead of strings) on change', waitForAsync(() => {
-    dispatchInputEvent(c.inputElement.nativeElement, '2020-12-31');
+    dispatchInputEvent(c.inputElement, '2020-12-31');
     expect(c.fixture.componentInstance.testDate).toEqual(jasmine.any(Date));
     expect(c.fixture.componentInstance.testDate).toEqual(new Date(2020, 11, 31));
     expect(c.fixture.componentInstance.testDate.getDate()).toBe(31);
