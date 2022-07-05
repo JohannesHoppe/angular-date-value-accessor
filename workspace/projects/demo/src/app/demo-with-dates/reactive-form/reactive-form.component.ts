a545243c6d2587881252af6069e20f9de5a5d84e
Ferdinand Malcher <ferdinand@malcher.media>
2022-07-05 19:08:51 +0200

    refactor: demo code with real FormGroup/FormControl

--- workspace/projects/demo/src/app/demo-with-dates/reactive-form/reactive-form.component.ts
+++ workspace/projects/demo/src/app/demo-with-dates/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 import { Release } from '../../shared/release';
 
@@ -7,44 +7,34 @@ import { Release } from '../../shared/release';
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html'
 })
-export class ReactiveFormComponent implements OnInit {
+export class ReactiveFormComponent {
 
-  demoDateValue: Release;
-  demoLocalDateValue: Release;
-  demoDefault: Release;
+  demoDateValue = new Release('2.0.0', new Date('2020-01-01')); // UTC
+  demoLocalDateValue = new Release('3.0.0', new Date(2020, 0, 1)); // with offset
+  demoDefault = new Release('1.5.8', new Date('2016-07-22')); // UTC
 
-  myFormDateValue: UntypedFormGroup;
-  myFormLocalDateValue: UntypedFormGroup;
-  myFormDefault: UntypedFormGroup;
+  myFormDateValue = new FormGroup({
+    version:     new FormControl(this.demoDateValue.version, { nonNullable: true }),
+    releaseDate: new FormControl(this.demoDateValue.releaseDate, { nonNullable: true })
+  });
 
-  constructor(private fb: UntypedFormBuilder) {
-    this.demoDateValue = new Release('2.0.0', new Date('2020-01-01')); // UTC
-    this.demoLocalDateValue = new Release('3.0.0', new Date(2020, 0, 1)); // with offset
-    this.demoDefault = new Release('1.5.8', new Date('2016-07-22')); // UTC
-  }
+  myFormLocalDateValue = new FormGroup({
+    version:     new FormControl(this.demoLocalDateValue.version, { nonNullable: true }),
+    releaseDate: new FormControl(this.demoLocalDateValue.releaseDate, { nonNullable: true })
+  });
 
-  ngOnInit() {
-    this.myFormDateValue = this.fb.group({
-      version:     [this.demoDateValue.version],
-      releaseDate: [this.demoDateValue.releaseDate]
-    });
+  myFormDefault = new FormGroup({
+    version:     new FormControl(this.demoDefault.version, { nonNullable: true }),
+    releaseDate: new FormControl(this.demoDefault.releaseDate, { nonNullable: true })
+  });
 
-    this.myFormLocalDateValue = this.fb.group({
-      version:     [this.demoLocalDateValue.version],
-      releaseDate: [this.demoLocalDateValue.releaseDate]
-    });
-
-    this.myFormDefault = this.fb.group({
-      version:     [this.demoDefault.version],
-      releaseDate: [this.demoDefault.releaseDate]
-    });
-
+  constructor() {
     this.myFormDateValue.valueChanges.subscribe(values => this.demoDateValue = new Release(values.version, values.releaseDate));
     this.myFormLocalDateValue.valueChanges.subscribe(values => this.demoLocalDateValue = new Release(values.version, values.releaseDate));
     this.myFormDefault.valueChanges.subscribe(values => this.demoDefault = new Release(values.version, values.releaseDate));
   }
 
-  toggle(formGroup: UntypedFormGroup) {
+  toggle(formGroup: FormGroup) {
     const control = formGroup.get('releaseDate');
     if (control.enabled) {
       control.disable();
